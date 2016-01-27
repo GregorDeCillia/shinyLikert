de48d8737ea011791fc1506d17c3412289ee7a56
Gregor de Cillia <de.cillia.gregor@gmail.com>
2016-01-27 16:40:23 +0100

    Remove unnecessary functions

--- R/plot.height.input.R
+++
@@ -1,9 +0,0 @@
-height.max              =  500
-height.min              =  200
-height.default          =  350
-
-plot.height.input = function( label ){
-  sliderInput( paste0( "plot.height.", label ),
-               "plot height",
-               height.min, height.max, height.default )
-}
