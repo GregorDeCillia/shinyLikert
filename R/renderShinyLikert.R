GregorDeCillia/shinyLikert

commit 51bfe1ffa7a26b47f120a872f6a352b64ae9f459

remove input and output arguents from renderShinyLikert and renderFactorOverview

R/renderShinyLikert.R
@@ -71,8 +71,11 @@ renderShinyLikert = function( data,
                               id = toString(paste0("id",
                                                    sample(1:10000, 1))),
                               ... ){
-  input = data$input
-  output = data$output
+  input  = get( 'input',   envir=env )
+  output = get( 'output',  envir=env )
+  data$input  = input
+  data$output = output
+
   if( is.null( input ) || is.null( output ) ){
     message("non interactive session\n")
     data = likert::likert( data$likert_data )$results
@@ -81,6 +84,13 @@ renderShinyLikert = function( data,
                   main = deparse( substitute( data ) ) )
       })
                   ) )
+  }
+
+  getInput = function( str, default = NULL ){
+    input_object = input[[ paste0( id, str ) ]]
+    if( is.null( input_object ) )   # shiny inputs get initialized as NULL
+      input_object = default        # since they are implemented like a list
+    input[[ paste0( id, str ) ]]
   }
 
   valid_factors = c( names(data$row_factors), names(data$"column_factors") )
@@ -96,9 +106,9 @@ renderShinyLikert = function( data,
   currentFactors = function(){
     out = NULL
     for ( factor in dropdown_factors ){
-      if( is.null( input[[ paste0( id, factor ) ]] ) )
+      if( is.null( getInput( factor ) ) )
         return( NULL )
-      out = c( out, input[[ paste0( id, factor ) ]] )
+      out = c( out, getInput( factor ) )
     }
     if ( is.null(out) )  ## bypass error messages by replacing NA with FALSE
       return ( NULL )
@@ -113,11 +123,11 @@ renderShinyLikert = function( data,
     )
     out$heightSlider = renderHeightSlider( id, height )
     if( ! is.null( split_factors )  ){
-      selection = input[[ paste0( id,".split_factors" )]]
+      selection = getInput( ".split_factors" )
       if( is.null(selection ) )
         selection = split_factors
       out$mulipanel = selectInput(
-        inputId = paste0( id,".split_factors" ),
+        inputId = paste0( id, ".split_factors" ),
         label   = "split factors",
         choices = setdiff(
           union(

R/createTestData.R
@@ -6,14 +6,11 @@
 #' distributions.
 #'
 #' @usage createTestData( participants, questions,
-#'                        input, output,
 #'                        participant_factors, question_factors,
 #'                        response_levels )
 #'
 #' @param participants  either the number of participants or a vector of participant names
 #' @param questions     either the number of questions or a vector of question names
-#' @param input         arguments for the shiny session
-#' @param output        arguments for the shiny session
 #' @param participant_factors a list containing all participant factors
 #' @param question_factors    a list containing all question factors
 #' @param response_levels     Vector with response levels
@@ -32,8 +29,6 @@
 #' @export
 createTestData = function( participants,
                            questions,
-                           input = NULL,
-                           output = NULL,
                            participant_factors = list( country = c("US","AT","CH"),
                                                        gender = c("Male", "Female" ),
                                                        position = c("Programmer",
@@ -84,9 +79,7 @@ createTestData = function( participants,
 
   out = list(likert_data = likert_data,
              row_factors = row_factors,
-             column_factors = column_factors,
-             input = input,
-             output  = output )
+             column_factors = column_factors )
   class( out ) = "likertData"
 
   return( out )

R/renderFactorOverview.R
@@ -2,8 +2,6 @@
 #'
 #' @param data   the datasetto be shown
 #' @param id     a unique id of the output
-#' @param input  argument for shiny session
-#' @param output argument for shiny session
 #' @param env    argument for shiny session
 #' @param quoted argument for shiny session
 #' @param expr   argument for shiny session
@@ -36,13 +34,13 @@ renderFactorOverview = function(
   data,
   id= toString(paste0("id",
                       sample(1:10000, 1))),
-  input = data$input,
-  output = data$output,
   env = parent.frame(),
   quoted = FALSE,
   expr= "NULL",
   ... )
 {
+  input  = get( 'input',   envir=env )
+  output = get( 'output',  envir=env )
   factor = reactive({
     input[[paste0(id,"factor")]]
   })

R/renderShinyPlot.R
@@ -10,6 +10,7 @@ renderShinyPlot = function( factors,
                             env = parent.frame(),
                             ...
 ){
+  getInput  = get( 'getInput',   envir=env )
   reactiveLikertTable = reactive({
     renderTestTable( filtered_data(),
                      likert_split, split_factors,
@@ -53,7 +54,7 @@ renderShinyPlot = function( factors,
     }
 
     if( !is.null( split_factors ) ){
-      split_factors = filtered$input[[paste0(id,".split_factors")]]
+      split_factors = getInput( ".split_factors" )
       # in case of a split-plot
       likert_table2 = create_factorized_table( filtered,
                                                split_factors,
@@ -84,9 +85,7 @@ renderShinyPlot = function( factors,
   height = function(){
     if( is.null( height ) )
       return( 450 )
-    high = filtered_data()$input[[ paste0( id, ".height" ) ]]
-    if( is.null( high ) )
-      return( 450 )
+    high = getInput( ".height", 450 )
     return ( high )
   }
   )

examples/chisqTest/chisq.Rmd
@@ -7,7 +7,7 @@ runtime: shiny
 
 ```{r, echo=FALSE, results ='hide', message=FALSE}
 library( shinyLikert )
-testData2 = createTestData( 100, 10, input, output )
+testData2 = createTestData( 100, 10 )
 ```
 
 
@@ -16,7 +16,6 @@ testData2 = createTestData( 100, 10, input, output )
       renderShinyLikert( 
         testData2,
         split    = c( "difficulty", "gender", "country" ),
-        positive.order = TRUE   # ???
       ) 
     )
 ```

examples/factorOverview/factorOverview.Rmd
@@ -9,10 +9,8 @@ runtime: shiny
 
   library( shinyLikert )
   testData2 = createTestData( participants = 100, 
-                              questions    = 10, 
-                              input, 
-                              output )
-  rendered = renderFactorOverview(testData2)
+                              questions    = 10 )
+  rendered = renderFactorOverview( testData2 )
   rendered$selector
   rendered$plot
 

examples/hierachicalFactors/hierachicalFactors.Rmd
@@ -10,9 +10,7 @@ Those examples will illustrate the use of hierachical dropdown menus. The import
 ```{r, message = FALSE, results='hide'}
   library(shinyLikert)      # load the package from installed library
   testData2 = createTestData( participants = 100, 
-                              questions    = 20, 
-                              input, 
-                              output )
+                              questions    = 20 )
 ```
 
 Now we will add the factor `question` to the `column_factors`
@@ -79,7 +77,7 @@ fluidPage(
 
 Note that if you select `skill` under `split_factors`, only one more bar will appear (or disappear if you unselect it). The other two possible levels are skipped because they contain no observations (given the `quesion`). I also want to mention, that in the above example, the choices of `country` are independent of the selections of `difficulty` and `question` since `country` is the only `row_factor` among those three factors.
 
-Plottingseveral items works as well. You will have to exclude `questions` from the `dropdown` input. Note that `split` does not work well with several questions. Therefore, we will replace it with `group`
+Plotting several items works as well. You will have to exclude `questions` from the `dropdown` input. Note that `split` does not work well with several questions. Therefore, we will replace it with `group`
 
 ```{r}
 fluidPage(

examples/misc/Examples.Rmd
@@ -19,9 +19,7 @@ To create a shiny widget the data is assumed to be of class `likertData`, which 
   #require( 'devtools' )
   #devtools::load_all( "../.." ) # load the package from source
   testData2 = createTestData( participants = 100, 
-                              questions    = 10, 
-                              input, 
-                              output )
+                              questions    = 10 )
 
 ```
 
@@ -147,11 +145,9 @@ shinyApp(
         uiOutput("selector"),
         uiOutput( "plot" )
       ),
-    server = function(input,output,session){
+    server = function( input, output, session ){
       testData3 = createTestData( participants = 100, 
-                                  questions    = 10, 
-                                  input, 
-                                  output )
+                                  questions    = 10  )
       rendered = renderShinyLikert( 
         testData3 ,
         dropdown = "gender" )

commit 2da07f1d2be7660734003174edf03803d0decb7a

Remove test for interactivity of session
This feature got broken by 51bfe1ff

R/renderShinyLikert.R
@@ -76,16 +76,6 @@ renderShinyLikert = function( data,
   data$input  = input
   data$output = output
 
-  if( is.null( input ) || is.null( output ) ){
-    message("non interactive session\n")
-    data = likert::likert( data$likert_data )$results
-    return( list( plot = renderPlot({
-      HH::likert( x = data,
-                  main = deparse( substitute( data ) ) )
-      })
-                  ) )
-  }
-
   getInput = function( str, default = NULL ){
     input_object = input[[ paste0( id, str ) ]]
     if( is.null( input_object ) )   # shiny inputs get initialized as NULL