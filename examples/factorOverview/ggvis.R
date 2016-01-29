928cf88a2c5a40cc75ced4c2d0a2fb02a835f6ae
Gregor de Cillia <de.cillia.gregor@gmail.com>
2016-01-29 09:34:10 +0100

    another example script producing ggvis plots

--- examples/factorOverview/ggvis.R
+++ examples/factorOverview/ggvis.R
@@ -20,3 +20,23 @@ plotGgvis = selected_data %>%
   hide_legend( "fill" )
 
 plotGgvis
+
+counts = as.numeric( table( selected_data ) )
+df = data.frame( levels= levels(selected_data[,1]), counts )
+
+plotGgvis = df %>%
+  ggvis( ~levels, ~counts ) %>%
+  layer_bars( fill = "sky blue",
+              fillOpacity := 0.7,
+              fillOpacity.hover := 0.9 ) %>%
+  add_axis( "y", title = "persons" ) %>%
+  add_axis( "x", title = "" ) %>%
+  add_tooltip( function( df ) {
+    if ( df$stack_upr_ > 1 )
+      paste0( df$stack_upr_, " persons" )
+    else
+      paste0( df$stack_upr_, " person" )
+  },
+  "hover" ) %>%
+  hide_legend( "fill" )
+
