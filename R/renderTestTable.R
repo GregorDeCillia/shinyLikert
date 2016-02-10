e7254da1673b41a704efd6a6f353b38343c664f0
Gregor de Cillia <de.cillia.gregor@gmail.com>
2016-02-10 01:18:23 +0100

    Change indents
    no functional changes

--- R/renderTestTable.R
+++ R/renderTestTable.R
@@ -14,36 +14,36 @@ renderTestTable = function( filtered,
     }
   }
 
-    if( !is.null( group ) ){
-      out = likert::likert(
-        filtered$likert_data,
-        grouping = filtered$row_factors[ ,group[1] ] )
-      out = cbind( out$results, p.value = NA )
-      for ( question in out$Item ){
-        p = test_function( filtered$likert_data[, question],
-                           filtered$row_factors[ ,group[1] ] )
-        out$p.value[ out$Item == question ] = p
+  if( !is.null( group ) ){
+    out = likert::likert(
+      filtered$likert_data,
+      grouping = filtered$row_factors[ ,group[1] ] )
+    out = cbind( out$results, p.value = NA )
+    for ( question in out$Item ){
+      p = test_function( filtered$likert_data[, question],
+                         filtered$row_factors[ ,group[1] ] )
+      out$p.value[ out$Item == question ] = p
+    }
+    out$Group[ out$Group == "NA" ] = NA
+    return( out )
+  }
+  if( !is.null( split_factors ) ){
+    if( dim(filtered$likert_data)[2]==0 )
+      return( NULL )
+    out = create_factorized_table( filtered,
+                                   split_factors )
+    out = cbind( out, p.value = NA )
+    for ( factor in split_factors )
+      if( factor %in% names( filtered$row_factors ) ){
+        p = test_function(
+          unlist( filtered$likert_data ),
+          rep( filtered$row_factors[,factor],
+               ncol( filtered$likert_data ) )
+        )
+        out$p.value[ out$factor == factor ] = p
       }
-      out$Group[ out$Group == "NA" ] = NA
-      return( out )
-    }
-    if( !is.null( split_factors ) ){
-      if( dim(filtered$likert_data)[2]==0 )
-        return( NULL )
-      out = create_factorized_table( filtered,
-                                     split_factors )
-      out = cbind( out, p.value = NA )
-      for ( factor in split_factors )
-        if( factor %in% names( filtered$row_factors ) ){
-          p = test_function(
-            unlist( filtered$likert_data ),
-            rep( filtered$row_factors[,factor],
-                 ncol( filtered$likert_data ) )
-          )
-          out$p.value[ out$factor == factor ] = p
-        }
-      return( out )
-    }
+    return( out )
+  }
 
-    create_table( filtered$likert_data )
+  create_table( filtered$likert_data )
 }
